277dbfb83539582e011f11065fcf7da0c3221eba
brad oyler <bradoyler@gmail.com>
2017-07-26 17:59:34 -0400

    1st pass at secureCheck (#7)
    * 1st pass at secureCheck
    
    * add html files to glob
    
    * fix url search in css
    
    * run secure-check after copyAssets
    
    * adds a friendly check mark

---
+++ bin/secureCheck.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const glob = require('glob')
+const log = require('log-utils')
+
+function secureCheck (files) {
+  const warnings = []
+  files.forEach(function (file) {
+    if (file.indexOf('node_modules') > -1) {
+      return
+    }
+    const content = fs.readFileSync(file, 'utf-8')
+    const lines = content.split(/\r?\n/g)
+
+    lines.forEach(function (line, i) {
+      var matches = line.match(/\w{3}(=|\()("|')http:\/\/[^\s'"]+/ig) || []
+      matches.forEach(function (match) {
+        match = { url: match, file, line: i + 1 }
+        if (match.url.match(/src=|url\(/ig)) {
+          console.log(`${match.file}:${match.line} -- ${match.url}`) // eslint-disable-line
+          warnings.push(match)
+        }
+      })
+    })
+  })
+  return warnings
+}
+
+const files = glob.sync('{,!(node_modules)/**/}*.{js,css,scss,hbs,html}')
+console.log(files.length, 'files checked for insecure assets!') // eslint-disable-line
+const warnings = secureCheck(files)
+if (warnings.length) {
+  throw Error('secure-check failed on:' + JSON.stringify(warnings))
+}
+log.ok('Assets look good')
