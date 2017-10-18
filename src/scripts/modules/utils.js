c4743f63fbdd0bf2b361947d7455f2d6f2081e30
Ian Rose <ian@ianrose.me>
2017-10-18 15:01:25 -0400

    mobile checking utils and request ani frame scroll util

--- src/scripts/modules/utils.js
+++ src/scripts/modules/utils.js
@@ -23,3 +23,50 @@ module.exports.debounce = function debounce (func, wait, immediate) {
     }
   }
 }
+
+// Example
+// if (utils.isMobile() { console.log('is mobile device')})
+module.exports.isMobile = function () {
+  let check = false
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    check = true
+  }
+  return check
+}
+
+// Example
+// if (utils.isAndroid() { console.log('is android device') })
+module.exports.isAndroid = function () {
+  return navigator.userAgent.match(/Android/i)
+}
+
+// Example
+// if (utils.isiPhone()) { console.log('is iphone device') }
+module.exports.isiPhone = function () {
+  return navigator.userAgent.match(/iPhone/i)
+}
+
+// Example:
+// utils.rAFscroll(drawLines)
+// via https://gist.github.com/bradoyler/6037548a8c132184476fb5374c327da7
+module.exports.rAFscroll = function (fn) {
+  const rAF = window.requestAnimationFrame ||
+               window.webkitRequestAnimationFrame ||
+               window.mozRequestAnimationFrame ||
+               window.msRequestAnimationFrame ||
+               window.oRequestAnimationFrame ||
+               // IE Fallback, you can even fallback to onscroll
+               function (callback) { window.setTimeout(callback, 1000 / 60) }
+
+  var lastPosition = -1
+  function loop () {
+    // Avoid calculations if not needed
+    if (lastPosition === window.pageYOffset) {
+      rAF(loop)
+      return false
+    } else lastPosition = window.pageYOffset
+    fn()
+    rAF(loop)
+  }
+  loop()
+}
