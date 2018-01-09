6ac35b850b5dbf5008ff67c4f601d51fb06bfc6d
Brad Oyler <bradoyler@gmail.com>
2018-01-09 17:20:54 -0500

    --build option

--- bin/build.js
+++ bin/build.js
@@ -29,187 +29,189 @@ const buildTasks = require('../gulp-tasks/build')
 const config = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'))
 const webpackConfig = require('../webpack.cli-config.js')(config)
 
-console.log('## DEBUG - projectName:', config.projectName, 'BUILD:', process.env.BUILD)
+module.exports = () => {
+  console.log('## DEBUG - projectName:', config.projectName, 'BUILD:', process.env.BUILD)
 
-// gulp rewriteAssetPath
-buildTasks.rewriteAssetPath(config)()
-// && gulp md5Assets
-buildTasks.md5Assets()
+  // gulp rewriteAssetPath
+  buildTasks.rewriteAssetPath(config)()
+  // && gulp md5Assets
+  buildTasks.md5Assets()
 
-function postBuild () {
-  // && gulp copyAssets
-  buildTasks.copyAssets()
+  function postBuild () {
+    // && gulp copyAssets
+    buildTasks.copyAssets()
 
-  // && gulp combineJson
-  buildTasks.combineJson(config)()
+    // && gulp combineJson
+    buildTasks.combineJson(config)()
 
-  // TODO: npm run secure-check
-}
-
-// start of 'npm start'
-// Set a true or false for production/development. Use to run certain plugins
-
-const BUILD = process.env.BUILD
-const BUILD_DEBUG = process.env.BUILD_DEBUG
-
-if (BUILD_DEBUG) {
-  process.env.DEBUG = 'metalsmith:*'
-}
-
-// Global Configuration
-const awsConfig = {
-  bucketName: process.env.BUCKET_NAME,
-  region: process.env.AWS_DEFAULT_REGION
-}
-
-const yearString = config.projectInitDate.year
-const monthString = config.projectInitDate.month
-const configProjectNameProp = config.projectName
-const convertedProjectName = configProjectNameProp.replace(/\s+/g, '-').toLowerCase()
-const objectsLocation = `machinist/dist/${yearString}/${monthString}/${convertedProjectName}/`
-const dateNow = new Date()
-const UTCDate = dateNow.toISOString()
-
-let assetPath = config.assetPath[BUILD] || '/'
-if (BUILD === 'production') {
-  if (config.assetPath.domain) {
-    assetPath = `${config.assetPath.domain}/${objectsLocation}`
-  } else if (!config.assetPath.production) {
-    assetPath = `//s3-${awsConfig.region}.amazonaws.com/${awsConfig.bucketName}/${objectsLocation}`
+    // TODO: npm run secure-check
   }
-}
 
-config.assetPath = assetPath
-config.version = pkg.version
-config.dependencies = pkg.dependencies
-// config.repository = pkg.repository.url
-config.devBuild = (BUILD === 'development')
-config.debugMode = (BUILD_DEBUG)
-config.dest = `./${config.dest}/`
-config.src = `./${config.src}/`
-config.buildDate = UTCDate
+  // start of 'npm start'
+  // Set a true or false for production/development. Use to run certain plugins
 
-// Adds metadata from files
-const data = {}
-const globalsDir = path.join(__dirname, '../src/data/globals/')
-if (fs.existsSync(globalsDir)) {
-  console.log('## DEBUG - globals/', globalsDir)
-  const dataFiles = fs.readdirSync(globalsDir)
-  dataFiles.forEach(function (filename) {
-    data[filename.split('.')[0]] = `data/globals/${filename}`
-  })
-} else {
-  console.log('## DEBUG - globals/', globalsDir, 'not found')
-}
+  const BUILD = process.env.BUILD
+  const BUILD_DEBUG = process.env.BUILD_DEBUG
 
-let ms = Metalsmith(path.join(__dirname, '../')) // move to root
+  if (BUILD_DEBUG) {
+    process.env.DEBUG = 'metalsmith:*'
+  }
 
-if (config.debugMode) {
-  debugUi.patch(ms) // http://localhost:3000/debug-ui/index.html
-}
+  // Global Configuration
+  const awsConfig = {
+    bucketName: process.env.BUCKET_NAME,
+    region: process.env.AWS_DEFAULT_REGION
+  }
 
-// Metalsmith Build
-ms.source(config.src)
-  .destination(config.dest)
-  .metadata(config)
-  .use(globaldata(data))
-  .use(models({
-    directory: `${config.src}data/models`
-  }))
-if (config.collections) {
-  ms.use(collections(config.collections))
-}
-ms.use(sass({
-  outputStyle: config.devBuild ? 'expanded' : 'compressed',
-  outputDir: 'styles',
-  sourceMapContents: config.devBuild,
-  sourceMapEmbed: config.devBuild
-}))
-  .use(postcss({
-    pattern: ['**/*.css', '!**/_*/*', '!**/_*'],
-    from: '*.scss',
-    to: '*.css',
-    map: config.devBuild ? {inline: true} : false,
-    plugins: {
-      'autoprefixer': {browsers: ['> 0.5%', 'Explorer >= 10']}
+  const yearString = config.projectInitDate.year
+  const monthString = config.projectInitDate.month
+  const configProjectNameProp = config.projectName
+  const convertedProjectName = configProjectNameProp.replace(/\s+/g, '-').toLowerCase()
+  const objectsLocation = `machinist/dist/${yearString}/${monthString}/${convertedProjectName}/`
+  const dateNow = new Date()
+  const UTCDate = dateNow.toISOString()
+
+  let assetPath = config.assetPath[BUILD] || '/'
+  if (BUILD === 'production') {
+    if (config.assetPath.domain) {
+      assetPath = `${config.assetPath.domain}/${objectsLocation}`
+    } else if (!config.assetPath.production) {
+      assetPath = `//s3-${awsConfig.region}.amazonaws.com/${awsConfig.bucketName}/${objectsLocation}`
     }
-  }))
-  .use(filedata({
-    pattern: ['styles/*.css']
-  }))
-  .use(webpack(webpackConfig))
-  .use(fingerprint({
-    pattern: 'styles/main.css',
-    keep: true
-  }))
-  .use(helpers({
-    directory: 'lib/helpers'
-  }))
-  .use(inplace({
-    engine: 'handlebars',
-    pattern: ['**/*.{html,xml,txt,md}', '!admin/*'],
-    directory: config.src,
-    partials: 'partials'
-  }))
-  .use(markdown({
-    smartypants: true,
-    gfm: true,
-    tables: true,
-    langPrefix: 'language-'
-  }))
-  .use(layouts({
-    engine: 'handlebars',
-    directory: 'layouts',
-    partials: 'partials',
-    default: 'story.hbs',
-    pattern: '**/*.html'
-  }))
-  .use(writemetadata({
-    pattern: ['embed.html', 'styles/*.css'],
-    ignorekeys: ['webpackStats'],
-    bufferencoding: 'utf8'
-  }))
-  .use(drafts())
-  .use(permalinks({
-    relative: false,
-    pattern: ':file',
-    linksets: [{
-      match: {collection: 'articles'},
-      pattern: ':collection/:title'
-    }, {
-      match: {collection: 'work'},
-      pattern: ':collection/:title'}
-    ]
-  }))
-  .use(paths({
-    property: 'paths',
-    directoryIndex: 'index.html'
-  }))
-  .use(raw())
-  .use(assets({
-    source: './assets', // relative to the working directory
-    destination: './assets' // relative to the build directory
-  }))
-
-if (config.devBuild) {
-  ms.use(browserSync({
-    server: config.dest,
-    files: [`${config.src}**/*.*`, 'layouts/*.*', 'partials/**/*.*', './config.yml', 'assets/**/*.*'],
-    open: false,
-    notify: false,
-    online: true
-  }))
-}
-
-ms.use(debug({
-  files: false,
-  match: '**/*.md'
-}))
-.build(function (error) {
-  if (error) {
-    console.log(error)
-    return
   }
-  console.log((config.devBuild ? 'Development' : 'Production'), 'build success, version', pkg.version)
-  postBuild()
-})
+
+  config.assetPath = assetPath
+  config.version = pkg.version
+  config.dependencies = pkg.dependencies
+  // config.repository = pkg.repository.url
+  config.devBuild = (BUILD === 'development')
+  config.debugMode = (BUILD_DEBUG)
+  config.dest = `./${config.dest}/`
+  config.src = `./${config.src}/`
+  config.buildDate = UTCDate
+
+  // Adds metadata from files
+  const data = {}
+  const globalsDir = path.join(__dirname, '../src/data/globals/')
+  if (fs.existsSync(globalsDir)) {
+    console.log('## DEBUG - globals/', globalsDir)
+    const dataFiles = fs.readdirSync(globalsDir)
+    dataFiles.forEach(function (filename) {
+      data[filename.split('.')[0]] = `data/globals/${filename}`
+    })
+  } else {
+    console.log('## DEBUG - globals/', globalsDir, 'not found')
+  }
+
+  let ms = Metalsmith(path.join(__dirname, '../')) // move to root
+
+  if (config.debugMode) {
+    debugUi.patch(ms) // http://localhost:3000/debug-ui/index.html
+  }
+
+  // Metalsmith Build
+  ms.source(config.src)
+    .destination(config.dest)
+    .metadata(config)
+    .use(globaldata(data))
+    .use(models({
+      directory: `${config.src}data/models`
+    }))
+  if (config.collections) {
+    ms.use(collections(config.collections))
+  }
+  ms.use(sass({
+    outputStyle: config.devBuild ? 'expanded' : 'compressed',
+    outputDir: 'styles',
+    sourceMapContents: config.devBuild,
+    sourceMapEmbed: config.devBuild
+  }))
+    .use(postcss({
+      pattern: ['**/*.css', '!**/_*/*', '!**/_*'],
+      from: '*.scss',
+      to: '*.css',
+      map: config.devBuild ? {inline: true} : false,
+      plugins: {
+        'autoprefixer': {browsers: ['> 0.5%', 'Explorer >= 10']}
+      }
+    }))
+    .use(filedata({
+      pattern: ['styles/*.css']
+    }))
+    .use(webpack(webpackConfig))
+    .use(fingerprint({
+      pattern: 'styles/main.css',
+      keep: true
+    }))
+    .use(helpers({
+      directory: 'lib/helpers'
+    }))
+    .use(inplace({
+      engine: 'handlebars',
+      pattern: ['**/*.{html,xml,txt,md}', '!admin/*'],
+      directory: config.src,
+      partials: 'partials'
+    }))
+    .use(markdown({
+      smartypants: true,
+      gfm: true,
+      tables: true,
+      langPrefix: 'language-'
+    }))
+    .use(layouts({
+      engine: 'handlebars',
+      directory: 'layouts',
+      partials: 'partials',
+      default: 'story.hbs',
+      pattern: '**/*.html'
+    }))
+    .use(writemetadata({
+      pattern: ['embed.html', 'styles/*.css'],
+      ignorekeys: ['webpackStats'],
+      bufferencoding: 'utf8'
+    }))
+    .use(drafts())
+    .use(permalinks({
+      relative: false,
+      pattern: ':file',
+      linksets: [{
+        match: {collection: 'articles'},
+        pattern: ':collection/:title'
+      }, {
+        match: {collection: 'work'},
+        pattern: ':collection/:title'}
+      ]
+    }))
+    .use(paths({
+      property: 'paths',
+      directoryIndex: 'index.html'
+    }))
+    .use(raw())
+    .use(assets({
+      source: './assets', // relative to the working directory
+      destination: './assets' // relative to the build directory
+    }))
+
+  if (config.devBuild) {
+    ms.use(browserSync({
+      server: config.dest,
+      files: [`${config.src}**/*.*`, 'layouts/*.*', 'partials/**/*.*', './config.yml', 'assets/**/*.*'],
+      open: false,
+      notify: false,
+      online: true
+    }))
+  }
+
+  ms.use(debug({
+    files: false,
+    match: '**/*.md'
+  }))
+  .build(function (error) {
+    if (error) {
+      console.log(error)
+      return
+    }
+    console.log((config.devBuild ? 'Development' : 'Production'), 'build success, version', pkg.version)
+    postBuild()
+  })
+}
