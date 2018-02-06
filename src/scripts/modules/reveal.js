6346beb81ac3bbf3f896ce311869b4344ee57c27
Ian Rose <ian@ianrose.me>
2018-02-06 15:07:54 -0500

    Cleanup and more examples rendering (#35)
    * updates modules, render more components,
    
    * readme update
    
    * update story.aml

--- src/scripts/modules/reveal.js
+++ src/scripts/modules/reveal.js
@@ -1,23 +1,29 @@
-var $offcanvasClose = $('[data-sset="offcanvas-close"]')
 
-$($offcanvasClose).click(function () {
-  $('.offcanvas.is-active').removeClass('is-active')
-})
+module.exports = function (revealBtn = '[data-sset="reveal"]', revealClose = '[data-sset="reveal-close"]', revealContainer = '.reveal__container') {
+  var $revealBtn = $(revealBtn)
+  var $revealClose = $(revealClose)
 
-var $revealBtn = $('[data-sset="reveal"]')
+  $revealBtn.click(function (e) {
+    e.preventDefault()
+    var revealSelector = $(this).data('id')
+    var $revealEl = $(`#${revealSelector}`)
+    var $revealElContainer = $(`#${revealSelector} ${revealContainer}`)
+    $revealEl.height($revealElContainer.outerHeight(true))
+    if ($revealEl.hasClass('is-active')) {
+      $revealEl.removeClass('is-active')
+      $revealEl.height(0)
+    } else {
+      $('.reveal.is-active').removeClass('is-active')
+      $revealEl.toggleClass('is-active')
+      $revealEl.height($revealElContainer.outerHeight(true))
+    }
+  })
 
-$revealBtn.click(function (e) {
-  e.preventDefault()
-  var revealSelector = $(this).data('id')
-  var $revealEl = $('#' + revealSelector)
-  var $revealElContainer = $('#' + revealSelector + ' ' + '.reveal__container')
-  $revealEl.height($revealElContainer.outerHeight(true))
-  if ($revealEl.hasClass('is-active')) {
+  $revealClose.click(function (e) {
+    e.preventDefault()
+    var revealSelector = $(this).data('id')
+    var $revealEl = $(`#${revealSelector}`)
     $revealEl.removeClass('is-active')
     $revealEl.height(0)
-  } else {
-    $('.reveal.is-active').removeClass('is-active')
-    $revealEl.toggleClass('is-active')
-    $revealEl.height($revealElContainer.outerHeight(true))
-  }
-})
+  })
+}
