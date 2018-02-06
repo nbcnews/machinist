6ab693ae7e1c4e4be43169aa34d20b8e5a28d06e
Ian Rose <ian@ianrose.me>
2018-02-06 17:20:29 -0500

    optional video props

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,4 @@
 // import './vendor/ai2html-resizer'
 // import utils from './modules/utils'
-import reveal from './modules/reveal'
-import offcanvas from './modules/offcanvas'
-reveal()
-offcanvas()
+// import reveal from './modules/reveal'
+// import offcanvas from './modules/offcanvas'
